2bdfbebc9ede89af118d375ef74142913f0b1ef6
guru1031 <dev.superstar1031@gmail.com>
2022-12-03 12:12:23 +0200

    test

--- look-at.js
+++ look-at.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", (event) => {
     var margT = $('a-scene').outerWidth(true) - $('a-scene').outerWidth();
-    alert(margT);
-    // $('.logo-img').css('left', $('a-scene')[0].clientWidth - 50);
+    // alert(margT);
+    $('.logo-img').css('left', $('a-scene')[0].clientWidth - 50);
 });
